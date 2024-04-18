cb7f6c78ad117ede8c332cdded747e9db0dde70b
sabrinasabat <sabrina.s354970@cesurformacion.com>
2024-04-18 09:54:25 +0200

    12-04

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,31 @@
 package main;
+import animales.dominio.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+    public static void main(String[] args){
+
+        // incluyendo los animales 'ave'
+        Ave ganso = new Ave();
+        Ave cuervo = new Ave();
+        Ave tucán = new Ave();
+        Ave albatros = new Ave();
+
+        // incluyendo los animales mamíferos
+        Mamifero caballo = new Mamifero();
+        Mamifero elefante = new Mamifero();
+        Mamifero gato = new Mamifero();
+        Mamifero jirafa = new Mamifero();
+
+        // incluyendo los animales reptiles
+        Reptil serpiente = new Reptil();
+        Reptil lagarto = new Reptil();
+        Reptil tortuga = new Reptil();
+        Reptil cocodrilos = new Reptil();
+
+
+    }
+
 }
